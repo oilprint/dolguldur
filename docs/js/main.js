4a4ca2b59d6368cf7aef982c2975a2bdb5793b51
oilprint <irynakozlova.k@gmail.com>
2023-05-27 21:31:48 +0300

    added mixitup

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,4 @@
 $(function () {
+  var mixer = mixitup('.works__content');
 
-  
 });
--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'  
   ])
   .pipe(concat('main.min.js')) 
